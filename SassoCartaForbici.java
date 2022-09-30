74f905e95d60a35fe9f3c755542d24ef81de9796
Felice Marano <felicemarano@icloud.com>
2022-09-30 11:06:04 +0200

    Funziona (a tratti). Delle volte va tutto ok però non capisco perchè delle volte non va, aggiustatelo

--- SassoCartaForbici.java
+++ SassoCartaForbici.java
@@ -1,9 +1,74 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SassoCartaForbici {
     public static void main(String[] args) {
         
-       benvenuto();
+        //HOME
+        benvenuto();
 
-       
+        //METODI
+        Scanner scan = new Scanner(System.in);
+
+        //ATTRIBUTI
+        String tuaMossa;
+        boolean decisione = true;
+        int pareggio = 0;
+        String sceltaPc;
+
+        System.out.println("Scrivi 1 per scegliere di giocare in modalità single-player:  ");
+        System.out.println("Scrivi 2 per scegliere di giocare in modalità CO-OP:  ");
+        System.out.print("Aspetto la tua modalità di gioco:  ");
+        int modalitàGioco = scan.nextInt();
+        
+
+
+        //CONTROLLO SCELTA UTENTE
+        if (modalitàGioco == 3) {
+
+            System.out.println("Scegli la modalità di gioco 1 e la modalità di gioco 2:  ");
+        } 
+
+
+       while(decisione) {
+
+        //MENU PRINCIPALE
+        switch (modalitàGioco) {
+            case 1:
+
+            System.out.print("Fai la tua mossa: ");
+            tuaMossa = scan.next();
+
+            sceltaPc = randomStringFromArr();
+            System.out.println("Ecco la mia mossa:  " + sceltaPc);
+
+            
+
+            if(tuaMossa.equals(sceltaPc)) {
+
+                pareggio =+ 1;
+                System.out.println("\nAbbiamo pareggiato...");
+
+            } else if ((tuaMossa.equals("sasso") && sceltaPc.equals("foribci" )) || (tuaMossa.equals("forbici") && sceltaPc.equals("carta")) || (tuaMossa.equals("carta") && sceltaPc.equals("sasso"))) {
+
+                System.out.println("Brav e vinciut");
+            } else {
+
+                System.out.println("Brav o strunz e perz");
+            }
+
+            default:
+                break;
+        }
+
+       }
+    }
+
+    public static String randomStringFromArr() {
+        String[] mosse = {"sasso", "carta", "forbici"};
+        int randIdx = ThreadLocalRandom.current().nextInt(mosse.length);
+        String randomElem = mosse[randIdx];
+        return randomElem;
     }
 
 
